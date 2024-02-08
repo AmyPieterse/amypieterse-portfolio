5b6bf4e53f7e095f40a7689f0e9e3bf30cffa623
amy <amypieterse.15@gmail.com>
2024-02-08 15:12:07 +0200

    resume

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,28 +1,21 @@
 <template>
-    <div class="container">   
-    <div class="row">
+    <div class="container">
         <div class="heading">
             <h2 class="text-center">Work & Education</h2>
-        </div>
+        </div>   
+    <div class="timeline-container">
         <div class="timeline">
             <ul>
-                <li v-for="work in resume" :key="work.id" class="timelineItem" data-aos="fade-up-right" data-aos-duration="800">
-                    <div class="timelineCard">
-                        <div class="timelineDate">{{work.year}}</div>
-                        <div class="timelineContent">
-                            <h3>{{work.place}}</h3>
-                            <p>{{work.work}}</p>
-                        </div>
+                <li v-for="work in resume" :key="work.id"  class="timeline-event" data-aos="fade-up-right" data-aos-duration="800">
+                    <label class="timeline-event-icon"></label>
+                    <div class="timeline-content">
+                        <p  class="timeline-event-thumbnail">{{work.year}}</p>
+                        <h3>{{work.place}}</h3>
+                        <p>{{work.work}}</p>
                     </div>
                 </li>
             </ul>
         </div>
-    </div>
-    <div class="row">
-        <div class="mb-4 mt-5">
-            <h3 class="text-center">Skills</h3>
-        </div>
-  
     </div>
 </div>
 </template>
@@ -41,21 +34,91 @@
 </script>
 
 <style scoped>
-.timeline{
+
+*{
     border: 2px solid aqua;
 }
-.timeline ul li{
-    margin: 5em auto;
-    border: 2px solid black;
-    width: 30rem;
-    min-height: 10rem;
-    text-align: center;
-    padding-top: auto;
-    padding-bottom: auto;
-    color: black;
+
+.timeline {
+  margin: 4em auto;
+  position: relative;
+  width: 80%;
+  max-width: 900px;
+  margin: 0 auto;
+
+  &:before {
+    background-color: black;
+    content: '';
+    margin-left: -1px;
+    position: absolute;
+    top: 0;
+    left: 2em;
+    width: 2px;
+    height: 100%;
+  }
 }
-.timelineContent{
-    margin-top: 1.5em;
+
+.timeline ul{
+    list-style: none;
+}
+
+
+
+.timeline-event {
+    width: 50%;
+    position: relative;
+  
+  &:hover {
+    .timeline-event-icon {
+      rotate:-45deg;
+      background-color: aqua;
+    }
+    
+    .timeline-event-thumbnail {
+      box-shadow:inset 40em 0 0 0 aqua;
+    }
+  }
+}
+
+.timeline-event:nth-child(odd){
+    float: left;
+    clear: right;
+} 
+.timeline-event:nth-child(even){
+    float: right;
+    clear: left;
+} 
+
+.timeline-content {
+  padding: 2em;
+  position: relative;
+  top: -1.875em;
+  left: 4em;
+  width: 80%;
+}
+
+.timeline-event-icon {
+  transition: transform 0.2s ease-in;
+  rotate: 45deg;
+  background-color: black;
+  outline: 6px solid white;
+  display: block;
+  margin: 0.5em 0.5em 0.5em -1.2em;
+  position: absolute;
+  top: 0;
+  width: 1em;
+  height: 1em;
+}
+
+.timeline-event-thumbnail {
+  transition: box-shadow 0.5s ease-in 0.1s;
+  color: white;
+  font-size: 0.75em;
+  background-color: black;
+  box-shadow: inset 0 0 0 0em #ef795a;
+  display: inline-block;
+  margin-bottom: 1.2em;
+  padding: 0.25em 1em 0.2em 1em;
 }
 
 </style>
